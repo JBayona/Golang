648849f1e53385be781a49d01542dbef5ee9015c
JBayona <jordavids_22@hotmail.com>
2018-09-08 12:09:00 -0700

    Struct functionStruct function..

---
+++ struct_function.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const value float64 = 65534
+const value2 float64 = 1.60934
+
+type car struct {
+	gasPedal      uint16
+	breakPedal    uint16
+	steeringWheel uint16
+	topSpeedKm    float64
+}
+
+func (c car) kmh() float64 {
+	return float64(c.gasPedal) * float64(c.topSpeedKm)
+}
+
+func main() {
+	fmt.Println("GO is working!")
+
+	aCar := &car{
+		gasPedal:      22341,
+		breakPedal:    0,
+		steeringWheel: 12561,
+		topSpeedKm:    225.0,
+	}
+	fmt.Println(aCar.gasPedal)
+	fmt.Println(aCar.breakPedal)
+	fmt.Println(aCar.steeringWheel)
+	fmt.Println(aCar.topSpeedKm)
+	fmt.Println("Function")
+	fmt.Println(aCar.kmh())
+}
