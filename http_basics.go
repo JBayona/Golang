eb2bedfc82844dfb0eb0b35fd84486546ff3f74f
JBayona <jordavids_22@hotmail.com>
2018-09-12 21:34:29 -0700

    Multilines.

--- http_basics.go
+++ http_basics.go
@@ -8,6 +8,10 @@ import (
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hey there</h1>")
 	fmt.Fprintf(w, "<p>You %s even add %s</p>", "can", "<strong>variables</strong>")
+
+	fmt.Fprintf(w,
+		`<p>Multines</p>
+		<p>Testing</p>`)
 }
 
 func main() {
